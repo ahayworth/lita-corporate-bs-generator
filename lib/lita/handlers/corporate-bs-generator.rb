f6a055f0a44f17e72bf04956493ea4dc01246b6e
Andrew Hayworth <ahayworth@gmail.com>
2019-10-30 23:18:34 -0500

    Update corporate-bs-generator.rb

--- lib/lita/handlers/corporate-bs-generator.rb
+++ lib/lita/handlers/corporate-bs-generator.rb
@@ -6,13 +6,13 @@ module Lita
     class CorporateBsGenerator < Handler
       route(/\bcorporate bs( please)?/i, :bs, :command => true, :help => { "corporate bs (please)" => "Spew some corporate garbage" })
 
-      BASE_URL = "http://cbsg.sourceforge.net/cgi-bin/live"
+      BASE_URL = "http://pasta.phyrama.com:8083/cgi-bin/live.exe"
 
       def bs(response)
         if (some_bs = redis.lpop("corporate-bs-generator"))
           response.reply some_bs
         else
-          data = Nokogiri::HTML.parse(http.post("http://cbsg.sourceforge.net/cgi-bin/live").body)
+          data = Nokogiri::HTML.parse(http.post("http://pasta.phyrama.com:8083/cgi-bin/live.exe").body)
           bs_list = data.xpath("//li").map(&:children).map { |x| x.text.strip }
           redis.lpush("corporate-bs-generator", bs_list)
           response.reply redis.lpop("corporate-bs-generator")
